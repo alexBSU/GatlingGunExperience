9b254b606c5356d5d3bea3862ed233d398a46976
alexBSU <alexBSU@users.noreply.github.com>
2019-12-11 16:58:51 -0700

    Readd Metadata and start of menu
    To fix metadate issues. Also has menu present in game world that is not functioning yet.

---
+++ Assets/GatlingGunExperience/Scripts/FaderScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FaderScript : MonoBehaviour
+{
+    [SerializeField]
+    Image fadeOutUIImage;
+    public Image blackout;
+    public float fadeSpeed = 0.3f;
+    public float fadeTime = 1.0f;
+    [SerializeField]
+    enum FadeDirection
+    {
+        In, //Alpha = 1
+        Out // Alpha = 0
+    }
+
+    private void Start()
+    {
+        if (fadeOutUIImage == null)
+        {
+            fadeOutUIImage = GameObject.FindWithTag("Fader").GetComponent<Image>();
+        }
+        if (blackout == null)
+        {
+            blackout = GameObject.FindWithTag("Blackout").GetComponent<Image>();
+        }
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(FadeDirection.Out));
+        Debug.Log("Fade Out");
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(Fade(FadeDirection.In));
+        Debug.Log("Fade In");
+    }
+
+    private IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+        float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+
+        //Original code
+        if (fadeDirection == FadeDirection.Out)
+        {
+            while (alpha >= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            fadeOutUIImage.enabled = false;
+        }
+        else
+        {
+            fadeOutUIImage.enabled = true;
+            while (alpha <= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+
+    /*public IEnumerator FadeAndLoadScene(FadeDirection fadeDirection, string sceneToLoad)
+    {
+        yield return Fade(fadeDirection);
+        SceneManager.LoadScene(sceneToLoad);
+    }*/
+    private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        fadeOutUIImage.color = new Color(fadeOutUIImage.color.r, fadeOutUIImage.color.g, fadeOutUIImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? -1 : 1);
+    }
+}
---
+++ Assets/GatlingGunExperience/Scripts/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    GameObject player;
+    FaderScript faderScript;
+    [SerializeField]
+    GameObject firingRangeSpawn;
+    [SerializeField]
+    GameObject museumSpawn;
+    [SerializeField]
+    GameObject assemblySpawn;
+    bool transportAllowed = false;
+
+    private void Awake()
+    {
+        faderScript = GameObject.FindWithTag("FadeScript").GetComponent<FaderScript>();
+        faderScript.enabled = false;
+        player = GameObject.FindWithTag("Player");
+        transportAllowed = true;
+    }
+
+    public void Transport(string location)
+    {
+        if (transportAllowed == true)
+        {
+            transportAllowed = false;
+            StartCoroutine(GotoLocation(location));
+        }
+        else
+        {
+            Debug.Log("Transport Error: Transport not allowed");
+        }
+    }
+
+    IEnumerator GotoLocation(string location)
+    {
+        faderScript.FadeIn();
+        yield return new WaitForSeconds(faderScript.fadeSpeed);
+        faderScript.blackout.enabled = true;
+
+        if (location == "Museum")
+        {
+            player.transform.position = museumSpawn.transform.position;
+            player.transform.rotation = museumSpawn.transform.rotation;
+        }
+        else if (location == "Assembly")
+        {
+            player.transform.position = assemblySpawn.transform.position;
+            player.transform.rotation = assemblySpawn.transform.rotation;
+        }
+        else if (location == "FiringRange")
+        {
+            player.transform.position = firingRangeSpawn.transform.position;
+            player.transform.rotation = firingRangeSpawn.transform.rotation;
+        }
+        else
+        {
+            Debug.Log("Transport Error: Transport location not found");
+        }
+
+        yield return new WaitForSeconds(faderScript.fadeTime - (faderScript.fadeSpeed*2));
+        faderScript.blackout.enabled = false;
+        faderScript.FadeOut();
+        yield return new WaitForSeconds(faderScript.fadeSpeed);
+        transportAllowed = true;
+    }
+}
