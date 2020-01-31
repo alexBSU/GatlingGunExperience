b8a617d0547e906c33058d0ddfc4713b2a66f8ca
CMRoss <ChrisRoss@u.boisestate.edu>
2020-01-31 08:53:38 -0700

    Added gunshot sound to the gun rotation and moved the scene into determined position

--- Assets/GatlingGunExperience/Scenes/Firing Range/FiringRangeScripts/BigWillyRotator.cs
+++ Assets/GatlingGunExperience/Scenes/Firing Range/FiringRangeScripts/BigWillyRotator.cs
@@ -5,18 +5,24 @@ public class BigWillyRotator : MonoBehaviour
 {
     public VRTK_BaseControllable controllable;
     public Transform ObjectToRotate;
+    public AudioSource gunSource;
 
     protected virtual void OnEnable()
     {
         controllable = controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable;
         controllable.ValueChanged += ValueChanged;
+        
     }
 
     protected virtual void ValueChanged(object sender, ControllableEventArgs e)
     {
         if (ObjectToRotate != null)
         {
-            ObjectToRotate.Rotate(0, 6, 0, Space.Self);
+            ObjectToRotate.Rotate(0, 15, 0, Space.Self);
+        }
+        if (ObjectToRotate.localEulerAngles.y % 15 == 0)
+        {
+            gunSource.Play();
         }
     }
 }
