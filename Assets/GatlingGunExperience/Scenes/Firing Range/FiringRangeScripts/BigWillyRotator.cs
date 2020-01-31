43fdc740c8b76ed95ce5ccb3fd94c5b26484c2ba
CMRoss <ChrisRoss@u.boisestate.edu>
2020-01-31 09:24:52 -0700

    added particle system muzzle flash

--- Assets/GatlingGunExperience/Scenes/Firing Range/FiringRangeScripts/BigWillyRotator.cs
+++ Assets/GatlingGunExperience/Scenes/Firing Range/FiringRangeScripts/BigWillyRotator.cs
@@ -6,12 +6,13 @@ public class BigWillyRotator : MonoBehaviour
     public VRTK_BaseControllable controllable;
     public Transform ObjectToRotate;
     public AudioSource gunSource;
+    public ParticleSystem gunFlash;
 
     protected virtual void OnEnable()
     {
         controllable = controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable;
         controllable.ValueChanged += ValueChanged;
-        
+        Debug.Log("Now have ref to " + gunFlash);
     }
 
     protected virtual void ValueChanged(object sender, ControllableEventArgs e)
@@ -22,6 +23,7 @@ public class BigWillyRotator : MonoBehaviour
         }
         if (ObjectToRotate.localEulerAngles.y % 15 == 0)
         {
+            gunFlash.Play();
             gunSource.Play();
         }
     }
