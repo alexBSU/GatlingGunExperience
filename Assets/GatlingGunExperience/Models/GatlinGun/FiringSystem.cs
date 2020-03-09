57d234585a291946028402e8494c65e8572ff4e2
CMRoss <ChrisRoss@u.boisestate.edu>
2020-03-08 18:38:45 -0600

    Bullet and Magazine inplemented, Casings shoot out the side when firing

---
+++ Assets/GatlingGunExperience/Models/GatlinGun/FiringSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringSystem : MonoBehaviour
+{
+    //public AudioSource gunSource;
+    public ParticleSystem gunFlash;
+    public GameObject spentBullet;
+    public AudioSource gunSoundSource;
+    public Transform bulletDrop;
+
+    //public float timeToLive = 3f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gunFlash.Play();
+        gunSoundSource.PlayOneShot(gunSoundSource.clip);
+        Instantiate(spentBullet, bulletDrop);
+        //spentBullet.GetComponent<Rigidbody>().velocity = new Vector3(-10, 0, -0);
+        
+        //Destroy(spentBullet, timeToLive);
+    }
+}
---
+++ Assets/TimeToLive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToLive : MonoBehaviour
+{
+    private Rigidbody bulletRB;
+
+    public float casingSpeed = 1;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        bulletRB = GetComponent<Rigidbody>();
+        //bulletRB.velocity = -transform.up * casingSpeed;
+        //bulletRB.velocity = -transform.right * casingSpeed;      
+        bulletRB.velocity = new Vector3(Random.Range(-1f, -0.5f), 0, Random.Range(-1, -0.5f)) * casingSpeed;     
+    }
+
+    private void FixedUpdate()
+    {
+        Destroy(this.gameObject, 5f);
+    }
+}
